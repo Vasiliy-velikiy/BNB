7e033038fa3b03444d88b948d662a97d2c229478
FaJlJleN@yandex.ru <FaJlJleN@ya.ru>
2021-11-01 23:23:41 +0300

    FactorialRecovery ver.1

---
+++ core-lib/src/main/java/com/datart/it/leaders/core/lib/model/factorial/FactorialNumber.java
@@ -0,0 +1,29 @@
+package com.datart.it.leaders.core.lib.model.factorial;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class FactorialNumber {
+    final LinkedList<Integer> inNumber;
+
+    public FactorialNumber (LinkedList<Integer> inNumber) {
+        //является числом в фк системе
+        this.inNumber = inNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FactorialNumber that = (FactorialNumber) o;
+        return Objects.equals(inNumber, that.inNumber);
+    }
+// equals сравнение значений с другим числом
+    //инкремент дикремент,
+// сложение сложение с другим числом столбиком
+// вычитание вычитание с другим числом
+    //
+    public int getSize() {
+        return inNumber.size();
+    }
+}
---
+++ core-lib/src/tests/com/datart/it/leaders/core/lib/model/factorial/FactorialRecoveryTest.java
@@ -0,0 +1,5 @@
+package com.datart.it.leaders.core.lib.model.factorial;
+
+class FactorialRecoveryTest {
+
+}
