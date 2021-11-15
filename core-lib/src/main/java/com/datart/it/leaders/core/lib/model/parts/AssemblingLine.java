43668da7ad358cf0b94460de8e417689d889b09a
Vasiliy Velikiy <moskalev36@yandex.ru>
2021-11-15 15:32:37 +0300

    fix part constructor and asmLine remove 1 element

--- core-lib/src/main/java/com/datart/it/leaders/core/lib/model/parts/AssemblingLine.java
+++ core-lib/src/main/java/com/datart/it/leaders/core/lib/model/parts/AssemblingLine.java
@@ -27,21 +27,18 @@ public class AssemblingLine {
 
     public List<Part> Process(int time) {  //время обработки процесса
         int t = 0;                      //нулевая метрика
-        int i = 0;                      //счетчик
         List<Part> finishedParts = new ArrayList<>();  //список завершенных деталей
         while (t <= time) {         //пока нулевая метрика меньше заданного времени выполняется цикл
-            Part curPart = asmLine.get(i);      //текущая деталь берется из начала списка
+            Part curPart = asmLine.remove(0)   ;   //текущая деталь берется из начала списка
             t += curPart.currentWorkflow.get(0).getTime(); //увеличивается метрика на время обработки на 1 станке (у детали может быть несколько станков, перед тем как она будет готова)
-            if (t > time) {                     //если метрика вышла за границу времени процесса
-                curPart.currentWorkflow.get(0).setTime(t - time); // в текущей детали на времени у 1станка устанавливается разность между метрикой и временем процесса
-                finishedParts.add(curPart);
-                break;
-            }
-            curPart.currentWorkflow.get(0).setTime(t);
+            if (t <= time) {                     //если метрика не вышла за границу времени процесса, добавляем готовую деталь в список готовых деталей
             finishedParts.add(curPart);
-            i++;
+            }
+            else{           //если метрика вышла за границу
+                curPart.currentWorkflow.get(0).setTime(t - time);// в текущей детали на времени у 1станка устанавливается разность между метрикой и временем процесса
+                           asmLine.add(0,curPart);   //закидываем деталь опять на линию но уже первой и с определенной разницей во времени
+            }
         }
         return finishedParts;
     }
-
 }
--- core-lib/src/main/java/com/datart/it/leaders/core/lib/model/parts/Part.java
+++ core-lib/src/main/java/com/datart/it/leaders/core/lib/model/parts/Part.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class Part{  //конкретная деталь которая запускаеися на конвеер прямо сейчас
     private int id; //индентификатор детали
     private PartType type; //тип детали
-    public ArrayList<Work> currentWorkflow;   //список станков, через которые должна пройти деталь
+    public List<Work> currentWorkflow;   //список станков, через которые должна пройти деталь
 
     public Part(int id, PartType type){
         this.id=id;
         this.type=type;
+    }
+
+    public Part(Integer id, PartType partType) {//инициализирую список станков текущей детали СПИСКОМ который должна пройти деаталь определенного типа чтобы быть готовой
+        this.currentWorkflow = new PartType(id).workflow;
     }
 
     public PartType getType() {
