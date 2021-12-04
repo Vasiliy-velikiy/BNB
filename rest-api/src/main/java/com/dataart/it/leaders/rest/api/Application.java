22be29cc8981adb0d5c608643fe038ddd7333466
Eugen <zheka461500@mail.ru>
2021-12-04 15:38:22 +0300

    Health check controller

--- rest-api/src/main/java/com/dataart/it/leaders/rest/api/Application.java
+++ rest-api/src/main/java/com/dataart/it/leaders/rest/api/Application.java
@@ -3,10 +3,11 @@ package com.dataart.it.leaders.rest.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dataart.it.leaders.rest.api.configuration")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
 
---
+++ rest-api/src/main/java/com/dataart/it/leaders/rest/api/service/HealthCheck.java
@@ -0,0 +1,5 @@
+package com.dataart.it.leaders.rest.api.service;
+
+public interface HealthCheck {
+    public String checkHealth();
+}
